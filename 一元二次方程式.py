df7370caf3c99f783edfd75b9272a7e4722351a9
user10470 <161004099+user10470@users.noreply.github.com>
2024-02-29 09:51:09 +0800

    Add files via upload

---
+++ 一元二次方程式.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 29 09:27:42 2024
+
+@author: student
+"""
+
+a = float(input('a ='))
+b = float(input('b ='))
+c = float(input('c ='))
+
+x1 = ((-b)+(b**2-4*a*c)**(0.5))/(2*a)
+x2 = ((-b)-(b**2-4*a*c)**(0.5))/(2*a)
+print("x1 =",x1)
+
+print("x2 =",x2)
